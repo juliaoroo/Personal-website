1936434cac5882210c06e65a01acfb2f990f52a1
juliaoroo <joronichm@edu.tecnocampus.cat>
2025-09-20 12:00:25 +0200

    commit 20: galeria pasable, enumeracio galeria i arreglar botons blog

---
+++ js/galerialist.js
@@ -0,0 +1,51 @@
+
+const imatges = [
+
+"../images/fotomuntanya1.jpg",
+"../images/fotomuntanya2.jpg",
+"../images/fotomuntanya3.jpg"
+
+];
+
+const FOTO_PER_PAGE = 4;
+let pagina = 1;
+
+const galeria = document.getElementById("galeria");
+const paginaActual = document.getElementById("pagina-actual");
+
+function mostrarImatges(){
+
+    galeria.innerHTML = "";
+    const inici = (pagina - 1) * FOTO_PER_PAGE;
+    const fi = inici + FOTO_PER_PAGE;
+    const imatgesPagina = imatges.slice(inici, fi);
+
+  imatgesPagina.forEach(src => {
+    const img = document.createElement("img");
+    img.src = src;
+    galeria.appendChild(img);
+  });
+
+  paginaActual.textContent = pagina + "/" + (FOTO_PER_PAGE/4);
+}
+
+// Botón anterior
+document.getElementById("anterior").addEventListener("click", () => {
+  if (pagina > 1) {
+    pagina--;
+    mostrarImatges();
+  }
+});
+
+// Botón siguiente
+document.getElementById("seguent").addEventListener("click", () => {
+  if (pagina < Math.ceil(imatges.length / FOTO_PER_PAGE)) {
+    pagina++;
+    mostrarImatges();
+  }
+});
+
+// Mostrar la primera página al cargar
+mostrarImatges();
+
+
